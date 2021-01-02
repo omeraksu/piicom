49dde7a72c0050089537e2be0c19c2344ad35ce3
erayklcc <erayklcc@icloud.com>
2021-01-02 16:06:27 +0300

    Bcrypt yapısı güncellendi

--- models/userModel.js
+++ models/userModel.js
@@ -1,44 +1,62 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
 
 
   email: {
     type: String,
-    required: [true,'Email is required.'],
+    required: [true, 'Email is required.'],
     unique: true,
     match: [
       /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
       // "Please Provide a Valid Email"
-  ]
+    ]
   },
   password: {
     type: String,
-    required: [true,'Password is required.'],
+    required: [true, 'Password is required.'],
     minlength: 6,
-    
-    
+
+
   },
   name: {
     type: String,
-    required:[true,'Name is required.']
+    required: [true, 'Name is required.']
   },
   isAdmin: {
     type: Boolean,
     default: false,
   },
   created: {
-    type:Date,
-    default:Date.now
+    type: Date,
+    default: Date.now
   },
-  profile_image:{
-    type:String,
-    default:"Default.jpg"
+  profile_image: {
+    type: String,
+    default: "Default.jpg"
   }
 
 });
+// Password Hsh
+userSchema.pre("save", function (next) {
+// Parola Değişme
+if(!this.isModified("password")){
+  next();
+}
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) next(err);
+      this.password = hash;
+      next();
+    })
+  })
+
+})
 
 const userModel = mongoose.model("User", userSchema);
 
-module.exports = userModel;
+module.exports = userModel
