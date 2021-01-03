490500f1f5091c951c87c62c98b4408d9d91965e
erayklcc <erayklcc@icloud.com>
2021-01-03 11:50:36 +0300

    Product Operasyonu Oturum Kontrolü ve Error Mekanizması Ok

--- middlewares/error/customErrorHandler.js
+++ middlewares/error/customErrorHandler.js
@@ -3,7 +3,7 @@ const CustomError = require("../../helpers/errors/CustomError")
 const customErrorHandler = (err, req, res, next) => {
   let customError = err;
   //Tanımsız Hata yakalamak için console.log ile hata yazdırılıp aşağıdaki örneklerle kendi mesajımzıa çevirilebilir
-
+console.log(err);
   if (err.name === "SyntaxError") {
     customError = new CustomError("Unexpected Syntax", 400);
   }
@@ -13,6 +13,9 @@ const customErrorHandler = (err, req, res, next) => {
   if (err.code === 11000) {
     //Duplicate Key Error
     customError = new CustomError("Duplicate Key Forund:Check Your Input")
+  }
+  if(err.name ==="TypeError"){
+    customError = new CustomError("Data is not found:Check your Input")
   }
 
   res
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const Product = require('../models/productModel');
-
-const getAccessToRoute  = require("../middlewares/authorization/auth")
-
+const getAccessToRoute  = require("../middlewares/authorization/auth");
+const CustomError = require("../helpers/errors/CustomError")
+const asyncErrorWrapper = require("express-async-handler");
 const router = express.Router();
 
 //Ürün getirme
 
-// router.get("/",getAccessToRoute,(req,res) =>{
+router.get("/",(req,res) =>{
    
     
-//     Product.find({},(error,product)=>{
-//         if(error){
-//             res.send("Product is not Found");
-//         }
-//         else{
-//             res.json(product);
-//         }
-//     })
+    Product.find({},(error,product)=>{
+        if(error){
+            res.send("Product is not Found");
+        }
+        else{
+            res.json(product);
+        }
+    })
     
-// })
+})
 
 
 
-//Ürün Ekleme //
-router.post('/',async (req, res) => {
+//Ürün Ekleme //Oturum Kontrolü ve Merkezi Hata Ok
+router.post("/newProduct",getAccessToRoute,asyncErrorWrapper(async (req, res,next) => {
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -37,22 +37,20 @@ router.post('/',async (req, res) => {
     });
     
     const newProduct = await product.save();
-    if (newProduct) {
+   
         return res
             .status(201)
             .send({ message: 'New Product created', data: newProduct });
-    }
-    return res
-        .status(500)
-        .send({ message: 'Error in Creating Product' });
-});
+    
+    
+}));
 
 // Ürün Güncelleme
-
-router.put('/:id',async (req, res) => {
+//Oturum Kontrolü ve Merkezi Hata Ok
+router.post('/update/:id',getAccessToRoute,asyncErrorWrapper(async (req, res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
-    if (product) {
+   
         product.name = req.body.name || product.name;
         product.price = req.body.price || product.price;
         product.image = req.body.image || product.image;
@@ -62,26 +60,25 @@ router.put('/:id',async (req, res) => {
         product.description = req.body.description || product.description;
 
         const updatedProduct = await product.save();
-        if (updatedProduct) {
+       
             return res
                 .status(200)
                 .send({ message: 'Product Updated', data: updatedProduct });
-        }
+        
 
-    }
-    return res.status(500).send({ message: 'Error in Updating Product.' });
-});
+    
+    
+}));
 
-// Ürün Silme //Eğer Admin İse
-router.delete('/:id',async (req, res) => {
+// Ürün Silme 
+//Oturum Kontrolü ve Merkezi Hata Ok
+router.delete('/delete/:id',getAccessToRoute,asyncErrorWrapper(async (req, res) => {
     const deletedProduct = await Product.findById(req.params.id);
-    if (deletedProduct) {
+    
         await deletedProduct.delete();
         res.send({ message: 'Product Deleted', });
-    } else {
-        res.send('Error in Deletion.');
-    }
-})
+   
+}))
 
 
 
